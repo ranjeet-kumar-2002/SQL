68481197e12ae0d2cb1fb8e530be85989684a4a4
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-14 20:21:05 +0530

    Update Constraints.sql

--- Structured  Query Language/Constraints.sql
+++ Structured  Query Language/Constraints.sql
@@ -1,3 +1,4 @@
+-- UNIQUE constrains : Ensures value in a column are unique across the table;
 CREATE DATABASE college;
 USE college;
 CREATE TABLE student(
@@ -7,3 +8,16 @@ CREATE TABLE student(
 INSERT INTO student(phone)
 VALUES 
 (1234);
+
+-- NOT NULL constrains : Ensures that column can not be null value;
+
+CREATE DATABASE college;
+USE college;
+CREATE TABLE student(
+age INT,
+rollno INT NOT NULL
+);
+
+INSERT INTO student(age , rollno)
+VALUES (27,121);
+
