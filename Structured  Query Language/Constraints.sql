27bd7d3249f0d89b3a70e36be7ce3d4a4e364b71
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-16 01:15:20 +0530

    Update Constraints.sql

--- Structured  Query Language/Constraints.sql
+++ Structured  Query Language/Constraints.sql
@@ -21,3 +21,36 @@ rollno INT NOT NULL
 INSERT INTO student(age , rollno)
 VALUES (27,121);
 
+-- check constraints enforce the condition for each row should be true
+
+CREATE DATABASE college;
+USE college;
+CREATE TABLE student(
+age INT CHECK(age>18)
+);
+INSERT INTO student(age)
+VALUES (33);
+
+-- DEFAULT CONSTRAINTS provides a default value is for col if value is not specified
+
+CREATE DATABASE IF NOT EXISTS college;
+USE college;
+CREATE TABLE IF NOT EXISTS student(
+schoolName VARCHAR(50) DEFAULT "XYZ",
+age INT
+);
+INSERT INTO student(age)
+VALUES (33);
+
+-- INT PRIMARY KEY (Uniuqe and not null)
+
+CREATE DATABASE IF NOT EXISTS college;
+USE college;
+CREATE TABLE IF NOT EXISTS student(
+age INT,
+rollno INT PRIMARY KEY
+);
+INSERT INTO student(age,rollno)
+VALUES (33,1),
+(12,3);
+
