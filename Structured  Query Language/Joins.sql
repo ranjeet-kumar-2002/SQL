ce20bc85cd2ede82789af9428262cbe4f9d0ceed
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-16 23:18:09 +0530

    Update Joins.sql

--- Structured  Query Language/Joins.sql
+++ Structured  Query Language/Joins.sql
@@ -1,3 +1,4 @@
+******************************* Inner join************************************************
 CREATE DATABASE IF NOT EXISTS ecom;
 
 USE ecom;
@@ -33,3 +34,46 @@ SELECT customer.id, orders.o_name, customer.name  -- for removing the replicated
 FROM customer
 INNER JOIN orders
 ON customer.id =  orders.id;
+
+****************************** Left join/ left outer join*************************************
+
+CREATE DATABASE IF NOT EXISTS ecom;
+
+USE ecom;
+
+CREATE TABLE IF NOT EXISTS customer (
+id INT,
+name VARCHAR(50)
+);
+
+INSERT INTO customer(id,name)
+VALUES (101,"Ram"),
+(102,"Rahul"),
+(103,"Riti");
+
+SELECT *FROM customer;
+
+USE ecom;
+CREATE TABLE IF NOT EXISTS orders (
+id INT PRIMARY KEY,
+o_name VARCHAR(50)
+);
+
+INSERT INTO orders(id,o_name)
+VALUES (102,"banana"),
+(103,"orange"),
+(104,"utensils");
+
+
+SELECT 
+*FROM customer
+LEFT JOIN orders
+ON customer.id = orders.id;
+
+SELECT customer.id, orders.o_name, customer.name  -- for removing the replicated col
+FROM customer
+LEFT JOIN orders
+ON customer.id = orders.id;
+
+DROP DATABASE ecom;
+
