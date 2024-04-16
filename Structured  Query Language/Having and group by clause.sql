a47a8d94ba55a821cc72c61f2d7f49185d56d8a3
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-16 20:08:25 +0530

    Update Having and group by clause.sql

--- Structured  Query Language/Having and group by clause.sql
+++ Structured  Query Language/Having and group by clause.sql
@@ -1,1 +1,24 @@
 
+CREATE DATABASE IF NOT EXISTS xyz; 
+USE xyz;
+CREATE TABLE IF NOT EXISTS Employee(
+id INT PRIMARY KEY,
+Department varchar(50),
+Salary INT
+);
+INSERT INTO Employee(id,Department,Salary)
+VALUES ( 1,"IT",1400),
+( 2,"IT",1300),
+( 5,"IT",1300),
+( 3,"SDE",5000),
+( 4,"MR",50000);
+
+-- GROUP BY clause 
+SELECT Department, AVG(Salary) AS avgsal FROM Employee  -- AS keyword does't  create a new physical col it will create new avgsal col 
+GROUP BY Department;
+
+-- having clause (Works on colleted/aggreated data with the condition
+
+SELECT Department, AVG(Salary) AS avgsal FROM Employee   
+GROUP BY Department
+HAVING avgsal>1500;
