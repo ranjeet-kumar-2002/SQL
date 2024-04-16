116e59be1f27c16c00553a27e4a7570af75042d7
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-16 23:35:32 +0530

    Update Joins.sql

--- Structured  Query Language/Joins.sql
+++ Structured  Query Language/Joins.sql
@@ -77,3 +77,43 @@ ON customer.id = orders.id;
 
 DROP DATABASE ecom;
 
+******************************* Right join/right outer join**************************8
+CREATE DATABASE IF NOT EXISTS ecom;
+
+USE ecom;
+
+CREATE TABLE IF NOT EXISTS customer (
+id INT,
+name VARCHAR(50)
+);
+
+INSERT INTO customer(id,name)
+VALUES (101,"Ram"),
+(102,"Rahul"),
+(103,"Riti");
+
+SELECT *FROM customer;
+
+USE ecom;
+CREATE TABLE IF NOT EXISTS orders (
+id INT PRIMARY KEY,
+o_name VARCHAR(50)
+);
+
+INSERT INTO orders(id,o_name)
+VALUES (102,"banana"),
+(103,"orange"),
+(104,"utensils");
+
+
+SELECT 
+*FROM customer
+RIGHT JOIN orders
+ON customer.id = orders.id;
+
+SELECT customer.id, orders.o_name, customer.name  -- for removing the replicated col
+FROM customer
+RIGHT JOIN orders
+ON customer.id = orders.id;
+
+DROP DATABASE ecom;
