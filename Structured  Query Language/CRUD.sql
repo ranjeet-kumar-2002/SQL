74b7fa2e3901f98a3f9d8456f1e27589acc8f4d2
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-04-16 13:03:32 +0530

    Update CRUD.sql

--- Structured  Query Language/CRUD.sql
+++ Structured  Query Language/CRUD.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS xyz;
+CREATE DATABASE IF NOT EXISTS xyz;  // create command
 USE xyz;
 
 CREATE TABLE IF NOT EXISTS Employee(
@@ -12,13 +12,30 @@ Salary INT
 
 INSERT INTO Employee(id,Name,age ,Department,City,Salary)
 VALUES ( 1,"Hello", 32,"IT","Delhi",5000000),
-( 2,"llo", 31,"SDE","felhi",50000000);
+( 2,"llo", 31,"SDE","felhi",50000000),
+( 3,"loo", 34,"SDE II","ddelhi",5000),
+( 5,"loo", 34,"SDE II","ddelhi",5000);
 
-UPDATE Employee
+UPDATE Employee    // update command
 SET Salary = 100000000
 WHERE Department = "IT";
 
+SET SQL_SAFE_UPDATES=0;  // for safe condion
+
+UPDATE Employee
+SET age = 111
+WHERE id = 1;
 SET SQL_SAFE_UPDATES=0;
+
+DELETE FROM Employee      // delete command
+WHERE Department = "IT";
+
+SELECT age,Name,Department FROM Employee;  // read the data
+
+SELECT *FROM Employee;
+
+DROP DATABASE xyz;
+
 
 UPDATE Employee
 SET age = 111
